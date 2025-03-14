4b6ac7d62ffbbb142e61b42f2738f8ee04b9ce07
Piotr <piotrek124@hotmail.com>
2025-03-14 10:31:10 +0100

    Dockerized MQTT broker & test microservice

---
+++ facial_recognition_service/app.js
@@ -0,0 +1,29 @@
+const mqtt = require('mqtt');
+
+
+console.log('Trying to connect to MQTT broker')
+const client = mqtt.connect('mqtt://MQTT:1883', {
+    username: 'facial_recognition_service',
+    password: 'admin123'
+});
+
+client.on('connect', () => {
+   console.log('connected to the broker')
+
+    client.subscribe('test/topic', (err) => {
+       if(err){
+           console.log('Error subsribing');
+           console.log(err);
+           return;
+       }
+       console.log('subscribed to test/topic');
+
+       client.publish('test/topic', 'message test');
+       console.log('message published');
+    });
+
+});
+
+client.on('message', (topic, message) => {
+    console.log(`message on ${topic}: ${message}`)
+});
