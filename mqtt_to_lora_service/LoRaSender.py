3bf1539a88dc4829da6f93574735e5aec6a97ea5
Piotr <piotrek124@hotmail.com>
2025-04-02 17:07:59 +0200

    mqtt_to_lora_service => lora => mqtt

--- mqtt_to_lora_service/LoRaSender.py
+++ mqtt_to_lora_service/LoRaSender.py
@@ -5,9 +5,26 @@ class LoRaSender(LoRa):
     def __init__(self, verbose=False):
         super(LoRaSender, self).__init__(verbose)
         self.set_mode(MODE.SLEEP)
-        self.set_dio_mapping([1, 0, 0, 0, 0, 0])
+        self.set_dio_mapping([0, 0, 0, 0, 0, 0])
+        self.rx_callback = None
+        lora.set_mode(MODE.RXCONT)
 
     def on_tx_done(self):
         print("Message sent.")
         self.clear_irq_flags(TxDone=1)
         self.set_mode(MODE.STDBY)
+
+    def on_rx_done(self):
+        self.clear_irq_flags(RxDone=1)
+        payload = self.read_payload(nocheck=True)
+        try:
+            message = bytes(payload).decode()
+            print(f"Received LoRa: {message}")
+            if self.rx_callback:
+                self.rx_callback(message)
+        except Exception as e:
+            print(f"Failed to decode payload: {e}")
+        self.set_mode(MODE.RXCONT)
+
+    def set_rx_callback(self, callback):
+        self.rx_callback = callback
--- mqtt_to_lora_service/app.py
+++ mqtt_to_lora_service/app.py
@@ -1,6 +1,8 @@
 from LoRaSender import  LoRaSender
 from config import BOARD
 from constants import *
+
+import threading
 import paho.mqtt.client as mqtt
 
 MQTT_USERNAME = 'mqtt_to_lora_service'
@@ -8,7 +10,6 @@ MQTT_PASSWORD = 'admin'
 MQTT_BROKER = 'accesscontrol.home'
 MQTT_PORT = 1883
 
-# Setup
 print('Board setup')
 BOARD.setup()
 print('Board reset')
@@ -19,7 +20,6 @@ lora = LoRaSender(verbose=False)
 print('lora standby')
 lora.set_mode(MODE.STDBY)
 
-# LoRa parameters (must match ESP)
 print('lora sets')
 lora.set_freq(433.0)
 lora.set_spreading_factor(7)
@@ -29,13 +29,13 @@ lora.set_preamble(8)
 lora.set_sync_word(0x12)
 lora.set_rx_crc(True)
 lora.set_pa_config(pa_select=1, max_power=7, output_power=14)
+lora.set_mode(MODE.RXCONT)
 
 print("LoRa Transmitter ready")
 
 def on_connect(client, userdata, flags, rc, properties=None):
     print(f"Connected with result code: {rc}")
     client.subscribe("lora/send/+/+")
-
 def on_message(client, userdata, msg):
     print(f"Message received on {msg.topic}")
 
@@ -66,4 +66,18 @@ def send_through_lora(device_id, command, payload):
     lora.write_payload(bytes)
     lora.set_mode(MODE.TX)
 
+def on_lora_message(message):
+    print(message)
+    message_parts = message.split(":")
+
+    device_id = message_parts[0]
+    topic = message_parts[1]
+    message = message_parts[2]
+
+    print(f"Sending on topic: {topic}")
+    print(f"Message: {message}")
+
+    client.publish(topic, message)
+
+lora.set_rx_callback(on_lora_message)
 client.loop_forever()
